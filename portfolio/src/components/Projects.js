1f1deb8cd341a61a10c152cd60ad0b5efde2e2fe
MaxOpper <maxwell8270@gmail.com>
2023-06-16 13:19:37 -0500

    first

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Projects.css';
-
+import opperstat from './images/opperstats.png'
+import tc from './images/tc.png'
+import cirke from './images/cirke.png'
 const Projects = () => {
   const handleOpperStatsClick = () => {
     // Handle the click event for OpperStats
@@ -9,7 +11,12 @@ const Projects = () => {
 
   const handleTrumanChatClick = () => {
     // Handle the click event for TrumanChat
-    window.open('https://example.com/trumanchat', '_blank');
+    window.open('https://maxopper.github.io/TrumanChat/', '_blank');
+  };
+
+  const handleCirkeClick = () => {
+    // Handle the click event for TrumanChat
+    window.open('https://maxopper.github.io/TrumanChat/', '_blank');
   };
 
   return (
@@ -17,13 +24,24 @@ const Projects = () => {
       <h2>Projects</h2>
       <div className="container">
         <div className="project-list">
-          <div className="project">
+          <div className="project opperstats">
             <button className="header-button" onClick={handleOpperStatsClick}>OpperStats</button>
             <p>OpperStats (Open-source Player Projection Estimation Report and Statistics System)</p>
+            <img src={opperstat} alt="Profile" />
+            <button className="read-button" onClick={handleOpperStatsClick}>Read More</button>
           </div>
-          <div className="project">
+          <div className="project trumanchat">
             <button className="header-button" onClick={handleTrumanChatClick}>TrumanChat</button>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Messaging app for Truman State University students</p>
+            <img src={tc} alt="Profile" />
+            <button className="read-button" onClick={handleOpperStatsClick}>Read More</button>
+          </div>
+          <div className="project opperstats">
+            <button className="header-button" onClick={handleOpperStatsClick}>CirkeBot</button>
+            <p>Cirke inspired Discord bot that responds to images</p>
+            <img src={cirke} alt="Profile" />
+            <div></div>
+            <button className="read-button" onClick={handleOpperStatsClick}>Read More</button>
           </div>
         </div>
       </div>
