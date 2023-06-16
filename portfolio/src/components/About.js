1ee615a4ebe10a48ff74cefc03a18b4349cbbe82
MaxOpper <maxwell8270@gmail.com>
2023-06-15 19:30:53 -0500

    tools

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,19 +1,64 @@
 // components/About.js
 import React from 'react';
 import './About.css';
-
+import pythonPNG from './images/python.png'
+import cppPNG from './images/cpp.png'
+import JSPNG from './images/JS.png'
+import RPNG from './images/R.png'
+import CSSPNG from './images/CSS.png'
+import HTMLPNG from './images/HTML.png'
 const About = () => {
   return (
     <section id="about" className="about">
-      <h2>Resume</h2>
-      <div className="container">
-        <p className="about-text">
-          I am a senior computer science student with a minor in statistics, on track to graduate at the end of the fall semester. I possess a strong passion for data science and software development. My goal is to leverage my technical skills and knowledge to contribute to innovative software projects. Equipped with a solid foundation in computer science and statistics, I am seeking opportunities to apply my skills and make a significant impact as a software developer.
-        </p>
-      </div>
+      <body>
+
+
+      <div class="row">
+  <div class="column">
+    <h2>Languages</h2>
+    <div class="language-item">
+  <div>
+    <img src={pythonPNG} alt="Python" />
+    <p>Python</p>
+  </div>
+  <div>
+    <img src={JSPNG} alt="Python" />
+    <p>JavaScript</p>
+  </div>
+  <div>
+    <img src={cppPNG} alt="CPP" />
+    <p>C++</p>
+  </div>
+  <div>
+    <img src={RPNG} alt="Python" />
+    <p>R</p>
+  </div>
+  <div>
+    <img src={HTMLPNG} alt="Python" />
+    <p>HTML</p>
+  </div>
+  <div>
+    <img src={CSSPNG} alt="Python" />
+    <p>CSS</p>
+  </div>
+</div>
+
+  </div>
+  <div class="column">
+    <h2>Tools</h2>
+    <p>Some text..</p>
+  </div>
+  <div class="column">
+    <h2>Education</h2>
+    <p>Some text..</p>
+  </div>
+</div>
+
+
+
+</body>
     </section>
   );
 }
-
 
 export default About;
