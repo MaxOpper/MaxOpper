4427d1feeb80799a9cfef53beda01a947cc6c8c3
MaxOpper <maxwell8270@gmail.com>
2023-06-20 16:40:21 -0500

    BWG

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -49,12 +49,14 @@ const Projects = () => {
             <button className="header-button" onClick={handleOpperStatsClick}>OpperStats</button>
             <p>OpperStats (Open-source Player Projection Estimation Report and Statistics System).</p>
             <img src={opperstat} alt="Profile" />
+            <div></div>
             <button className="read-button" onClick={handleReadStat}>Read More</button>
           </div>
           <div className="project trumanchat">
             <button className="header-button" onClick={handleTrumanChatClick}>TrumanChat</button>
             <p>Messaging app for Truman State University students.</p>
             <img src={tc} alt="Profile" />
+            <div></div>
             <button className="read-button" onClick={handleReadChat}>Read More</button>
           </div>
           
@@ -68,12 +70,14 @@ const Projects = () => {
             <button className="header-button" onClick={handlePortClick}>Portfolio</button>
             <p>The site you are currently on!</p>
             <img src={port} alt="Profile" />
+            <div></div>
             <button className="read-button" onClick={handleReadPort}>Read More</button>
           </div>
           <div className="project trumanchat">
             <button className="header-button" onClick={handleHeardClick}>HeardIT</button>
             <p>Spiritual Successor to Heardle.io</p>
             <img src={heard} alt="Profile" />
+            <div></div>
             <button className="read-button" onClick={handleReadHeard}>Read More</button>
           </div>
           
