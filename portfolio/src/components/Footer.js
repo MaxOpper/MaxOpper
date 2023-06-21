4ba70eb921fe92e56b93db59bc0fd9366b735e2e
MaxOpper <maxwell8270@gmail.com>
2023-06-21 13:22:36 -0500

    colored buttons

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,13 +1,39 @@
 // components/Footer.js
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.css';
 
 const Footer = () => {
+  const [mode, setMode] = useState('light'); // Default mode is light
+
+  const handleModeChange = (newMode) => {
+    setMode(newMode);
+    console.log(newMode);
+  };
+
   return (
-    <footer className="footer">
+    <footer className={`footer ${mode}`}>
       <div className="container">
-        <p>&copy; 2023 Maxwell Opper</p>
+        
+        <div className="mode-switcher">
+          <div
+            className={`mode-switcher-circle light ${mode === 'light' ? 'active' : ''}`}
+            onClick={() => handleModeChange('light')}
+          ></div>
+          <div
+            className={`mode-switcher-circle dark ${mode === 'dark' ? 'active' : ''}`}
+            onClick={() => handleModeChange('dark')}
+          ></div>
+          <div
+            className={`mode-switcher-circle pink ${mode === 'pink' ? 'active' : ''}`}
+            onClick={() => handleModeChange('pink')}
+          ></div>
+          <div
+            className={`mode-switcher-circle blue ${mode === 'blue' ? 'active' : ''}`}
+            onClick={() => handleModeChange('blue')}
+          ></div>
+        </div>
       </div>
+      <p>&copy; 2023 Maxwell Opper</p>
     </footer>
   );
 }
